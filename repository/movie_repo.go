5f9a7089cec1d71f7e4046a1a20d5c7d55577fd0
Alina <tikhomirova60@gmail.com>
2026-08-27 12:06:23 +0300

    genres in all movies

--- repository/movie_repo.go
+++ repository/movie_repo.go
@@ -11,16 +11,19 @@ import (
 
 func (r movieRepository) FindAllMovies() ([]models.MovieDto, error) {
 	query := `
-		SELECT m.id, m.title, m.release_year, m.duration, a.id, a.name, a.birth_date
+		SELECT m.id, m.title, m.release_year, m.duration, a.id, a.name, a.birth_date, g.id, g.name
 		FROM movies m
 		LEFT JOIN movie_actors ma ON ma.movie_id = m.id
 		LEFT JOIN actors a ON a.id = ma.actor_id
+		LEFT JOIN genres_movies gm ON gm.movie_id = m.id
+		LEFT JOIN genres g ON g.id = gm.genre_id
 	`
 
 	rows, err := r.db.Query(query)
 	if err != nil {
 		return []models.MovieDto{}, fmt.Errorf("%w: something happened during query execution: %w", m.ErrInternalIssue, err)
 	}
+	defer rows.Close()
 
 	moviesMap := make(map[int]*models.MovieDto)
 
@@ -34,6 +37,9 @@ func (r movieRepository) FindAllMovies() ([]models.MovieDto, error) {
 		var actorName sql.NullString
 		var actorBirthDate sql.NullString
 
+		var genreID sql.NullInt64
+		var genreName sql.NullString
+
 		err := rows.Scan(
 			&movieID,
 			&title,
@@ -42,6 +48,8 @@ func (r movieRepository) FindAllMovies() ([]models.MovieDto, error) {
 			&actorID,
 			&actorName,
 			&actorBirthDate,
+			&genreID,
+			&genreName,
 		)
 
 		if err != nil {
@@ -62,11 +70,40 @@ func (r movieRepository) FindAllMovies() ([]models.MovieDto, error) {
 		}
 
 		if actorID.Valid {
-			movie.Actors = append(movie.Actors, m.ActorInFilmDto{
-				Id:        int(actorID.Int64),
-				Name:      actorName.String,
-				BirthDate: actorBirthDate.String,
-			})
+			actorExists := false
+
+			for _, actor := range movie.Actors {
+				if actor.Id == int(actorID.Int64) {
+					actorExists = true
+					break
+				}
+			}
+
+			if !actorExists {
+				movie.Actors = append(movie.Actors, m.ActorInFilmDto{
+					Id:        int(actorID.Int64),
+					Name:      actorName.String,
+					BirthDate: actorBirthDate.String,
+				})
+			}
+		}
+
+		if genreID.Valid {
+			genreExists := false
+
+			for _, genre := range movie.Genres {
+				if genre.Id == int(genreID.Int64) {
+					genreExists = true
+					break
+				}
+			}
+
+			if !genreExists {
+				movie.Genres = append(movie.Genres, m.GenreWithoutMovies{
+					Id:   int(genreID.Int64),
+					Name: genreName.String,
+				})
+			}
 		}
 	}
 
